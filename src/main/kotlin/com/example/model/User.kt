6c0df31ebc3b0d1d090cacf0cc5522f808b340cb
Gerson Chelengo <bonymubay@gmail.com>
2024-02-18 23:46:32 +0200

    add table for database

--- src/main/kotlin/com/example/model/User.kt
+++ src/main/kotlin/com/example/model/User.kt
@@ -1,7 +1,19 @@
 package com.example.plugins.model
 
+import org.jetbrains.exposed.sql.*
 import kotlinx.serialization.Serializable
+
 
 @Serializable
 data class User(val nome: String, val morada: String,val idade: Int )
+
+object user: Table (){
+    val id = integer("id")
+    val nome= varchar("nome",100)
+    val morada =varchar("morada",100)
+    val idade = integer("idade")
+
+ override   val primaryKey = PrimaryKey(id)
+
+}
 
